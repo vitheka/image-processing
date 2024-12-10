5dab119f298274c29c8be3c2ccdecc296896cd34
Vitor Da Silveira Coelho <vitorcoelhoft@gmail.com>
2024-12-10 19:21:13 -0300

    First commit

---
+++ src/test/java/br/com/vitheka/image_processing/ImageProcessingApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.vitheka.image_processing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageProcessingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
